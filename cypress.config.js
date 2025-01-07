5b2af2227f9a052a5672fbcc9ef6d85d6e3be33c
Willian Neves Arruda <willian.arruda@ablewise.pt>
2025-01-07 21:19:22 +0000

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,32 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+    mochaJunitReporterOptions: { 
+      mochaFile: 'cypress/reports/junit/results-[hash].xml'
+    },
+    cypressMochawesomeReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
+  },
+  defaultCommandTimeout: 12000, // Tempo padrão para espera de comandos (15 segundos)
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on)
+      // Implementar escutadores de eventos aqui, se necessário
+    }
+  },
+  component: {
+    setupNodeEvents(on, config) {
+      // Implementar escutadores de eventos aqui, se necessário
+    }
+  }
+});
