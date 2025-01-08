c8a927b99ba9827b9405d970849705ff89c2e5e3
Willian Neves Arruda <willian.arruda@ablewise.pt>
2025-01-08 10:45:13 +0000

    morechangeees

--- cypress.config.js
+++ cypress.config.js
@@ -1,44 +1,30 @@
 const { defineConfig } = require("cypress");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
 
 module.exports = defineConfig({
   screenshotQuality: 80,
   video: false, // Desabilitar vídeos
-  screenshotOnRunFailure: true, // Ativar capturas de tela em falhas
-  reporter: "cypress-multi-reporters", // Usar múltiplos reportes
+  screenshotOnRunFailure: true, // Capturas de tela em falhas
+  reporter: "cypress-mochawesome-reporter", // Usar apenas Mochawesome
   reporterOptions: {
-    reporterEnabled: "cypress-mochawesome-reporter, mocha-junit-reporter", // Definir Mochawesome e JUnit
-    mochaJunitReporterOptions: {
-      mochaFile: "cypress/reports/junit/results-[hash].xml", // Relatório JUnit
-    },
-    cypressMochawesomeReporterOptions: {
-      reportDir: "cypress/reports/html", // Diretório para relatórios html e json
-      charts: true, // Incluir gráficos no relatório
-      reportPageTitle: "Relatório de testes",
-      embeddedScreenshots: true, // Incluir capturas de tela no relatório
-      inlineAssets: true, // Incluir assets inline
-      saveAllAttempts: false, // Salvar apenas o último teste
-      overwrite: false, // Não sobrescrever relatórios anteriores
-      html: true, // Gerar relatório HTML
-      json: true, // Gerar relatório JSON
-      disableScreenshots: false, // Garantir que as capturas de tela sejam incluídas
-      disableVideos: true, // Desabilitar vídeos
-    },
+    reportDir: "cypress/reports/html", // Diretório dos relatórios
+    charts: true,
+    reportPageTitle: "Relatório de Testes",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: false,
+    html: true,
+    json: true,
   },
-  screenshotsFolder: "cypress/screenshots", // Diretório para capturas de tela
-  videosFolder: "cypress/videos", // Diretório para vídeos (se habilitado)
+  screenshotsFolder: "cypress/screenshots", // Diretório das capturas de tela
+  videosFolder: "cypress/videos", // Diretório dos vídeos
   defaultCommandTimeout: 15000,
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
-    video: false, // Desabilitar vídeo para os testes
     setupNodeEvents(on, config) {
-      // Configurar o plugin Mochawesome
-      require("cypress-mochawesome-reporter/plugin")(on);
-    },
-  },
-  component: {
-    setupNodeEvents(on, config) {
-      // Configuração para testes de componentes (se houver)
+      mochawesome(on); // Configurar o Mochawesome
+      return config;
     },
   },
 });
