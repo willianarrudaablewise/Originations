9dfb1d86ef2d8e751215a41c09e02f38b2afad5b
Willian Neves Arruda <willian.arruda@ablewise.pt>
2025-01-08 04:17:44 +0000

    teste

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
+const { defineConfig } = require('cypress'); // Importar corretamente
+
 module.exports = defineConfig({
   screenshotQuality: 80,
-  video: false,
+  video: false, // Configurado como false pelo cypress.json
   screenshotOnRunFailure: true,
   reporter: "cypress-multi-reporters",
   reporterOptions: {
@@ -9,13 +11,13 @@ module.exports = defineConfig({
       mochaFile: "cypress/reports/junit/results-[hash].xml",
     },
     cypressMochawesomeReporterOptions: {
-      reportDir: "cypress/reports/html", // Diretório de relatórios (html e json)
+      reportDir: "cypress/reports/html", // Diretório do relatório
       charts: true,
       reportPageTitle: "Relatório de testes",
       embeddedScreenshots: true,
       inlineAssets: true,
       saveAllAttempts: false,
-      overwrite: false,
+      overwrite: false, // Adicionado do cypress.json
       html: true,
       json: true,
       disableScreenshots: true,
@@ -28,12 +30,14 @@ module.exports = defineConfig({
   viewportWidth: 1280,
   viewportHeight: 720,
   e2e: {
-    video: false,
+    video: false, // Configuração de vídeo ajustada do cypress.json
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
   component: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      // Configuração de eventos para testes de componentes
+    },
   },
 });
