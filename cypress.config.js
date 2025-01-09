3c21e698d1dcdea82e5abf6f09c5e37feb0fa5ad
Willian Neves Arruda <willian.arruda@ablewise.pt>
2025-01-09 02:01:03 +0000

    relatorio

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,5 @@
 const { defineConfig } = require("cypress");
 const mochawesome = require("cypress-mochawesome-reporter/plugin");
-const fs = require("fs");
-const path = require("path");
 
 module.exports = defineConfig({
   screenshotQuality: 80,
@@ -26,30 +24,6 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       mochawesome(on); // Configurar o Mochawesome
-
-      // Após cada execução de teste, excluir o screenshot anterior e manter apenas o novo
-      on('after:screenshot', (details) => {
-        const screenshotsDir = path.dirname(details.path); // Diretório onde o screenshot foi salvo
-        const screenshotFiles = fs.readdirSync(screenshotsDir); // Lista todos os arquivos no diretório de screenshots
-        
-        // Filtra para pegar apenas arquivos PNG (capturas de tela)
-        const screenshotImages = screenshotFiles.filter(file => file.endsWith('.png'));
-
-        if (screenshotImages.length > 1) {
-          // Ordena os arquivos de captura de tela pela data de modificação (do mais recente para o mais antigo)
-          const sortedFiles = screenshotImages.sort((a, b) => {
-            return fs.statSync(path.join(screenshotsDir, b)).mtime.getTime() - fs.statSync(path.join(screenshotsDir, a)).mtime.getTime();
-          });
-
-          // Exclui todos os screenshots antigos, mantendo apenas o mais recente
-          sortedFiles.slice(1).forEach(file => {
-            fs.unlinkSync(path.join(screenshotsDir, file)); // Exclui o screenshot antigo
-          });
-        }
-        
-        return null; // Retorna null para continuar a execução do Cypress
-      });
-
       return config;
     },
   },
