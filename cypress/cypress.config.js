8e8d43e6b23fa0988afc23e994f5425201d2f26a
Willian Neves Arruda <willian.arruda@ablewise.pt>
2025-04-02 10:26:50 +0100

    atualizado

---
+++ cypress/cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require("cypress");
+const mochawesome = require("cypress-mochawesome-reporter/plugin");
+
+module.exports = defineConfig({
+  screenshotQuality: 100,
+  video: true, // Desabilitar vídeos
+  screenshotOnRunFailure: true, // Capturas de tela em falhas
+  reporter: "cypress-mochawesome-reporter", // Usar apenas Mochawesome
+  reporterOptions: {
+    reportDir: "cypress/reports/html", // Diretório dos relatórios (ajustado para ser o mesmo que o YAML)
+    charts: true,
+    reportPageTitle: "Relatório de Testes",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    overwrite: true,
+    html: true,
+    json: true,
+  },
+  screenshotsFolder: "cypress/reports/screenshots", // Diretório das capturas de tela
+  videosFolder: "cypress/reports/videos", // Diretório dos vídeos
+  defaultCommandTimeout: 15000,
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  e2e: {
+    retries: 1, // Tenta novamente até 2 vezes se falhar
+    setupNodeEvents(on, config) {
+      mochawesome(on); // Configurar o Mochawesome
+      return config;
+    },
+  },
+});
